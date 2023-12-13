d6e9ff78140dcc99debe1af75e5e750657fcb651
Belamir26 <pybl2001@gmail.com>
2023-12-13 16:41:42 -0500

    Solo funciona el lector de "Y"

--- GY89_V01/src/main.cpp
+++ GY89_V01/src/main.cpp
@@ -1,18 +1,50 @@
 #include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
+#include <Wire.h>
+  //Este sensor hace dos cosas accelerometro y magnetismo
+//Objetos
+Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(0x1D);
 
-// put function declarations here:
-int myFunction(int, int);
+//Para mostar lo del sensor en el serial 
+void disAtSerial(void){
+  sensor_t sensor;
+  accel.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" m/s^2");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" m/s^2");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" m/s^2");
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial.println("Accelerometer Test"); Serial.println("");
+  Wire.end();
+  Wire.begin();
+  accel.begin();
+  disAtSerial();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.print("Here we go:  ");
+  sensors_event_t event;
+
+  accel.getEvent(&event); 
+
+
+  // Este es la medida.. en XYZ
+  Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  ");Serial.println("m/s");
+
+
+  //Pilas que tambien puedo acquirir el valor crudo o RAW.. 
+  delay(2000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
